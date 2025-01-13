8f5a10661436e01a83028d45ff9ec24867ed09f3
Samandar Nemataliyev <samandarnemataliyev@gmail.com>
2025-01-13 17:23:39 +0500

    initial commit

---
+++ project.py
@@ -0,0 +1,37 @@
+"""
+Proyekt mavzusi:   *** Shifoxona Menejment Tizimi ***
+
+Funksiyalari:
+    Shifokorlar va bemorlarni ro'yxatga olish.
+    Xonalarni boshqarish va bandlik holatini kuzatish.
+    Bemorlar uchun uchrashuvlarni rejalashtirish.
+    Bemorlar holati va davolanish tarixini ko'rish.
+    Shifokorlar, bemorlar va uchrashuvlar ro'yxatini ko'rsatish.
+
+Klasslari:
+    Doctor: Shifokorlar haqida ma'lumotni saqlash.
+    Patient: Bemorlar haqida ma'lumotni saqlash.
+    Appointment: Uchrashuvlar haqida ma'lumotni boshqarish.
+    HospitalManager: Shifoxona tizimini boshqaruvchi funksiyalarni o'z ichiga olgan klass.
+
+Project shartlari:
+    1.Clean code
+    2.magic methodlardan foydalanish kerak
+    3.decorator
+    4.class atribute
+"""
+
+class Doctor:
+    pass
+
+
+class Patient:
+    pass
+
+
+class Appointment:
+    pass
+
+
+class HospitalManager:
+    pass
